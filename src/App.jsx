4e835c187da870b53690f322e9bd7adcefe349b0
ErekRaph <westdrex@gmail.com>
2024-07-18 12:51:06 +0100

    done with this

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import Navbar from "./Components/navbar";
+// import Navbar from "./Components/navbar";
 import HeroImage from "./Components/hero-image";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <HeroImage />
     </>
   );
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -6,19 +6,41 @@ import "../Components/navbar.css";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <nav className="navbar-container">
-  <img src={logo} alt="logo" className="logo" />
+  return (
+    <nav className="navbar-container">
+      <img src={logo} alt="logo" className="logo" />
 
-  <ul className="navbar">
-    <li><a href="#" class="active">Home</a></li>
-    <li><a href="#" class="active">Recipe</a></li>
-    <li><a href="#" class="active">Order</a></li>
-    <li><a href="#" class="active">Blog</a></li>
-  </ul>
+      <ul className="navbar">
+        <li>
+          <a href="#" class="active">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" class="active">
+            Recipe
+          </a>
+        </li>
+        <li>
+          <a href="#" class="active">
+            Order
+          </a>
+        </li>
+        <li>
+          <a href="#" class="active">
+            Blog
+          </a>
+        </li>
+      </ul>
 
-  <div className="nav-button">
-    <button className="user"><img src={user} alt="user" /> Sign Up</button>
-    <button className="cart"><img src={cart} alt="cart" /> Cart</button>
-  </div>
-</nav>
+      <div className="nav-button">
+        <button className="user">
+          <img src={user} alt="user" /> Sign Up
+        </button>
+        <button className="cart">
+          <img src={cart} alt="cart" /> Cart
+        </button>
+      </div>
+    </nav>
+  );
 }
