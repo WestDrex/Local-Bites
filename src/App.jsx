57cf6f08724de3bea2fc7672295896c1aa73d9df
1C0DER <106778213+1C0DER@users.noreply.github.com>
2024-07-18 12:38:32 +0100

    howItWorks birth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Navbar from "./Components/navbar";
+import HowItWorks from "./Components/how-it-works";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <HowItWorks />
     </>
   );
 }
