e0dd7ce21d456ac307abc2939472f7d8cc5e626d
ErekRaph <westdrex@gmail.com>
2024-07-18 10:52:23 +0100

    Done the background image, search bar and text for the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Navbar from "./Components/navbar";
+import HeroImage from "./Components/hero-image";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <HeroImage />
     </>
   );
 }
---
+++ src/Components/hero-image.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./home";
+export default function HeroImage() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
