5fd9d2b86d27fe4e41edb3c8cf4f2b5d48eaa780
1C0DER <106778213+1C0DER@users.noreply.github.com>
2024-07-25 10:09:06 +0100

    God help us pls

---
+++ src/Pages/BlogPageComponents/BlogHero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Search from "../../assets/Images/srchButton.png";
+import "../BlogPageComponents/BlogHero.css";
+
+import { Link } from "react-router-dom";
+
+export default function BlogHero() {
+    return (
+        <div className="BlogHeroConatiner">
+            <div className="BlogHeroContent">
+                <h1>Exposing the Beauty and 
+                    Culture<br/> of Nigeria through 
+                    delicious <br/>Cuisine</h1>
+                <form className="blogSearch" action="">
+                    <img className="searchButton" src={Search} alt="search-button" />
+                    <input className="blogSearchText" type="text" placeholder="Search Articles" name="search"/>
+                    <button className="blogSearchButton" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    );
+}
