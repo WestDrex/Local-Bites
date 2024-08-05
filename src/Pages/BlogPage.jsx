f86605c60b14bec36bf0c1caa5fe0a82abcdd3ed
ErekRaph <westdrex@gmail.com>
2024-08-05 08:57:53 +0100

    since you want me to push

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import BlogBackground from "./BlogPageComponents/BlogBackground";
 import BlogPopularArticles from "./BlogPageComponents/BlogPopularArticles";
 import BlogLatestArticles from "./BlogPageComponents/BlogLatestArticles";
+import InfoBanner from "./BlogPageComponents/infobanner";
 // import "../Styles/RecipePage.css";
 
 export default function BlogPage() {
   return (
     <div>
       <BlogBackground />
-      <BlogPopularArticles/>
-      <BlogLatestArticles/>
+      <BlogPopularArticles />
+      <BlogLatestArticles />
+      <InfoBanner />
     </div>
   );
 }
