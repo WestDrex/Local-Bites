4332d41137446abe6480f083c6f2569718ef1353
ErekRaph <westdrex@gmail.com>
2024-08-06 08:17:40 +0100

    changes

--- src/Components/eatlocaldata.jsx
+++ src/Components/eatlocaldata.jsx
@@ -1,8 +1,13 @@
+import jollofrice from "../assets/Images/jollof.png";
+import yamporr from "../assets/Images/porridge.png";
+import suyaa from "../assets/Images/suya.png";
+import nkwobi from "../assets/Images/nkwobi.png";
 const eatLocalData = [
   {
     id: 1,
     name: "Jollof Rice",
-    image: "../src/assets/Images/jollof.png",
+    // image: "../src/assets/Images/jollof.png",
+    image: jollofrice,
     description:
       "Historians believe the dish originated in Senegal and Gambia during colonial rule in West Africa between 1860 and 1940.",
     rating: 5,
@@ -10,7 +15,7 @@ const eatLocalData = [
   {
     id: 2,
     name: "Yam Porridge",
-    image: "../src/assets/Images/porridge.png",
+    image: yamporr,
     description:
       "This originated from Ijebu tribe located in Ogun State, the meal has gained popularity and is made by many till this day.",
     rating: 4,
@@ -18,7 +23,7 @@ const eatLocalData = [
   {
     id: 3,
     name: "Suya: Beef Kebab",
-    image: "../src/assets/Images/suya.png",
+    image: suyaa,
     description:
       "This is believed to have originated with the Hausa people in Northern Nigeria, but it is now popular all over Nigeria especially in Lagos.",
     rating: 5,
@@ -26,7 +31,7 @@ const eatLocalData = [
   {
     id: 4,
     name: "Nkwobi: Spicy Cow",
-    image: "../src/assets/Images/nkwobi.png",
+    image: nkwobi,
     description:
       "It is basically a delicious meal of cow meat prepared in spicy palm oil sauce and garnished with utazi or rahanu originated in Igbo.",
     rating: 4,
