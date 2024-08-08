211b8b1d9c8124602709c9cb73e04da7ac49d9c1
ErekRaph <westdrex@gmail.com>
2024-08-08 11:39:52 +0100

    should be working now

--- src/categoriesdata.jsx
+++ src/categoriesdata.jsx
@@ -2,33 +2,38 @@ const CategoriesData = [
   {
     id: 1,
     // imgSrc: "../src/assets/Images/staple.png",
-    imgSrc: "../public/Images/staple.png",
+    imgSrc:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723112705/staple_konejq.png",
     foodname: "Staple Foods",
   },
   {
     id: 2,
     // imgSrc: "../src/assets/Images/soups.png",
-    imgSrc: "../public/Images/soups.png",
+    imgSrc:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723112705/soups_yhmhu4.png",
     foodname: "Soups/Stews",
   },
   {
     id: 3,
     // imgSrc: "../src/assets/Images/salad.png",
-    imgSrc: "../public/Images/salad.png",
+    imgSrc:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723113468/salad_zymafn.png",
     foodname: "Salads/Sides",
   },
 
   {
     id: 4,
     // imgSrc: "../src/assets/Images/kebab.png",
-    imgSrc: "../public/Images/kebab.png",
+    imgSrc:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723113518/kebab_fbi7n6.png",
     foodname: "Street Foods",
   },
 
   {
     id: 5,
     // imgSrc: "../src/assets/Images/snacks.png",
-    imgSrc: "../public/Images/snacks.png",
+    imgSrc:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723112729/snacks_tc3eyi.png",
     foodname: "Snacks",
   },
 ];
--- src/placesdata.jsx
+++ src/placesdata.jsx
@@ -12,21 +12,24 @@ const placesData = [
     id: 2,
     name: "Abuja",
     // image: "../src/assets/Images/abuja.png",
-    image: "../../public/Images/abuja.png",
+    image:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723112706/abuja_pfdexq.png",
     rating: 4,
   },
   {
     id: 3,
     name: "Uyo",
     // image: "../src/assets/Images/uyo.png",
-    image: "../../public/Images/uyo.png",
+    image:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723112706/uyo_irebwp.png",
     rating: 5,
   },
   {
     id: 4,
     name: "Calabar",
     // image: "../src/assets/Images/calabar.png",
-    image: "../../public/Images/calabar.png",
+    image:
+      "https://res.cloudinary.com/dyntb0gv5/image/upload/v1723112822/calabar_dvny3a.png",
     rating: 4,
   },
 ];
